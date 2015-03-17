bac21562f9353331b83c7c029070f91c06f7fb3c
mariashka <bisyarinamariashka@gmail.com>
2015-03-17 20:36:14 +0300

    Fix bug with checking args

--- src/ru/ifmo/ctddev/bisyarina/implementor/Implementor.java
+++ src/ru/ifmo/ctddev/bisyarina/implementor/Implementor.java
@@ -3,6 +3,7 @@ package ru.ifmo.ctddev.bisyarina.implementor;
 import info.kgeorgiy.java.advanced.implementor.ImplerException;
 
 import java.io.File;
+import java.lang.System;
 
 /**
  * The {@link Implementor} class provides
@@ -14,25 +15,33 @@ public class Implementor {
     }
 
     /**
-     * Generates jar-file containing implementation of a given class.
+     * Generates jar-file containing implementation of a given class to given jar file.
      * Prints a message in case of an error.
      *
-     * @param args String array containing name of the class to implement
+     * @param args String array containing "-jar" option, name of the class to implement,
+     * jar file to generate
      */
 
     public static void main(String[] args) {
-        if (args == null || args.length == 0 || args[0] == null) {
+        if (args == null || args.length < 3 || args[0] == null) {
             System.err.println("Invalid args");
             return;
         }
-        try {
-            Class<?> cl = Class.forName(args[0]);
-            Implementation impl = new Implementation();
-            impl.implementJar(cl, new File("."));
-        } catch (ClassNotFoundException e) {
-            System.err.println("Invalid class");
-        } catch (ImplerException e) {
-            System.err.println(e.getMessage());
+
+        if (args[0].equals("-jar")) {
+            if (args[1] == null || args[2] == null) {
+                System.err.println("Invalid args");
+                return;
+            }
+            try {
+                Class<?> cl = Class.forName(args[1]);
+                Implementation impl = new Implementation();
+                impl.implementJar(cl, new File(args[2]));
+            } catch (ClassNotFoundException e) {
+                System.err.println("Invalid class");
+            } catch (ImplerException e) {
+                System.err.println(e.getMessage());
+            }
         }
     }
 }
