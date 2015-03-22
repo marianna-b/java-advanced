6d6a392c0bc8f70441bbda511132e54cbe2cf0db
mariashka <bisyarinamariashka@gmail.com>
2015-03-22 15:02:28 +0300

    Add javadoc

--- src/ru/ifmo/ctddev/bisyarina/concurrent/ParallelList.java
+++ src/ru/ifmo/ctddev/bisyarina/concurrent/ParallelList.java
@@ -10,110 +10,165 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+/**
+ * {@link ru.ifmo.ctddev.bisyarina.concurrent.ParallelList} provides functionality for
+ * list parallel computations
+ */
+
 public class ParallelList implements ListIP {
-    @Override
+    /**
+     * Returns maximum element of list. If it is not unique - the first
+     * @param i amount of threads to use
+     * @param list list to process
+     * @param comparator comparator to determine maximum
+     * @param <T> supertype of elements of the list
+     * @return maximum element
+     * @throws InterruptedException if any thread computing result has interrupted the current thread.
+     */
     public <T> T maximum(int i, List<? extends T> list, Comparator<? super T> comparator) throws InterruptedException {
         BiFunction<T, T, T> f = (a, b) -> comparator.compare(a, b) < 0 ? b : a;
-        BiFunction<Integer, Integer, Supplier<T>> g = (l, r) -> () -> {
+        Function<List<? extends T>, Supplier<T>> g = (subList) -> () -> {
             Monoid<T> monoid = new Monoid<>(f);
-            for (int j = l; j < r; j++) {
-                monoid.process(list.get(j));
-            }
+            subList.forEach(monoid::process);
             return monoid.get();
         };
-        ParallelInvoker<T> parallelInvoker = new ParallelInvoker<>(i, list.size(), g);
-        return parallelInvoker.getAll(new Monoid<>(f));
+        ParallelInvoker<T> parallelInvoker = new ParallelInvoker<>(i, list, g);
+        return parallelInvoker.get(new Monoid<>(f));
     }
 
-    @Override
+    /**
+     * Returns minimum element of list. If it is not unique - the first
+     * @param i amount of threads to use
+     * @param list list to process
+     * @param comparator comparator to determine minimum
+     * @param <T> supertype of elements of the list
+     * @return minimum element
+     * @throws InterruptedException if any thread computing result has interrupted the current thread.
+     */
     public <T> T minimum(int i, List<? extends T> list, Comparator<? super T> comparator) throws InterruptedException {
         BiFunction<T, T, T> f = (a, b) -> comparator.compare(a, b) > 0 ? b : a;
-        BiFunction<Integer, Integer, Supplier<T>> g = (l, r) -> () -> {
+        Function<List<? extends T>, Supplier<T>> g = (subList) -> () -> {
             Monoid<T> monoid = new Monoid<>(f);
-            for (int j = l; j < r; j++) {
-                monoid.process(list.get(j));
-            }
+            subList.forEach(monoid::process);
             return monoid.get();
         };
-        ParallelInvoker<T> parallelInvoker = new ParallelInvoker<>(i, list.size(), g);
-        return parallelInvoker.getAll(new Monoid<>(f));
+        ParallelInvoker<T> parallelInvoker = new ParallelInvoker<>(i, list, g);
+        return parallelInvoker.get(new Monoid<>(f));
     }
 
-    @Override
+    /**
+     * Returns if all elements suit predicate
+     * @param i amount of threads to use
+     * @param list list to process
+     * @param predicate predicate to suit
+     * @param <T> supertype of elements of the list
+     * @return true, if all elements suit predicate, false otherwise
+     * @throws InterruptedException if any thread computing result has interrupted the current thread.
+     */
     public <T> boolean all(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
         BiFunction<Boolean, Boolean, Boolean> f = (a, b) -> a&b;
-        BiFunction<Integer, Integer, Supplier<Boolean>> g = (l, r) -> () -> {
+        Function<List<? extends T>, Supplier<Boolean>> g = (subList) -> () -> {
             Monoid<Boolean> monoid = new Monoid<>(f, true);
-            for (int j = l; j < r; j++) {
-                monoid.process(predicate.test(list.get(j)));
+            for (T element : subList) {
+                monoid.process(predicate.test(element));
             }
             return monoid.get();
         };
 
-        ParallelInvoker<Boolean> parallelInvoker = new ParallelInvoker<>(i, list.size(), g);
-        return parallelInvoker.getAll(new Monoid<>(f, true));
+        ParallelInvoker<Boolean> parallelInvoker = new ParallelInvoker<>(i, list, g);
+        return parallelInvoker.get(new Monoid<>(f, true));
     }
 
-    @Override
+    /**
+     * Returns if any element suits predicate
+     * @param i amount of threads to use
+     * @param list list to process
+     * @param predicate predicate to suit
+     * @param <T> supertype of elements of the list
+     * @return true, if exists element suiting predicate, false otherwise
+     * @throws InterruptedException if any thread computing result has interrupted the current thread.
+     */
     public <T> boolean any(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
         BiFunction<Boolean, Boolean, Boolean> f = (a, b) -> a|b;
-        BiFunction<Integer, Integer, Supplier<Boolean>> g = (l, r) -> () -> {
+        Function<List<? extends T>, Supplier<Boolean>> g = (subList) -> () -> {
             Monoid<Boolean> monoid = new Monoid<>(f, false);
-            for (int j = l; j < r; j++) {
-                monoid.process(predicate.test(list.get(j)));
+            for (T element : subList) {
+                monoid.process(predicate.test(element));
             }
             return monoid.get();
         };
-        ParallelInvoker<Boolean> parallelInvoker = new ParallelInvoker<>(i, list.size(), g);
-        return parallelInvoker.getAll(new Monoid<>(f, false));
+        ParallelInvoker<Boolean> parallelInvoker = new ParallelInvoker<>(i, list, g);
+        return parallelInvoker.get(new Monoid<>(f, false));
     }
 
-    @Override
+    /**
+     * Returns string representation of values in list
+     * @param i amount of threads to use
+     * @param list list to process
+     * @return string representation of list elements
+     * @throws InterruptedException if any thread computing result has interrupted the current thread.
+     */
     public String concat(int i, List<?> list) throws InterruptedException {
         BiFunction<StringBuilder, StringBuilder, StringBuilder> f = StringBuilder::append;
-        BiFunction<Integer, Integer, Supplier<StringBuilder>> g = (l, r) -> () -> {
+        Function<List<?>, Supplier<StringBuilder>> g = (subList) -> () -> {
             StringBuilder builder = new StringBuilder();
-            for (int j = l; j < r; j++) {
-                builder.append(list.get(j).toString());
+            for (Object aSubList : subList) {
+                builder.append(aSubList.toString());
             }
             return builder;
         };
-        ParallelInvoker<StringBuilder> parallelInvoker = new ParallelInvoker<>(i, list.size(), g);
-        return parallelInvoker.getAll(new Monoid<>(f)).toString();
+        ParallelInvoker<StringBuilder> parallelInvoker = new ParallelInvoker<>(i, list, g);
+        return parallelInvoker.get(new Monoid<>(f)).toString();
     }
 
-    @Override
+    /**
+     * Returns list of elements that suit predicate
+     * @param i amount of threads to use
+     * @param list list to process
+     * @param predicate predicate to suit
+     * @param <T> supertype of elements of the list
+     * @return list of elements that suit predicate
+     * @throws InterruptedException if any thread computing result has interrupted the current thread.
+     */
     public <T> List<T> filter(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
         BiFunction<List<T>, List<T>, List<T>> f = (a, b) -> {
             a.addAll(b);
             return a;
         };
-        BiFunction<Integer, Integer, Supplier<List<T>>> g = (l, r) -> () -> {
+        Function<List<? extends T>, Supplier<List<T>>> g = (subList) -> () -> {
             List<T> res = new ArrayList<>();
-            for (int j = l; j < r; j++) {
-                if (predicate.test(list.get(j)))
-                    res.add(list.get(j));
+            for (T element : subList) {
+                if (predicate.test(element))
+                    res.add(element);
             }
             return res;
         };
-        ParallelInvoker<List<T>> parallelInvoker = new ParallelInvoker<>(i, list.size(), g);
-        return parallelInvoker.getAll(new Monoid<>(f));
+        ParallelInvoker<List<T>> parallelInvoker = new ParallelInvoker<>(i, list, g);
+        return parallelInvoker.get(new Monoid<>(f));
     }
 
-    @Override
+    /**
+     * Returns list of elements generated by applying given function to list elements
+     * @param i amount of threads to use
+     * @param list list to process
+     * @param function function to apply
+     * @param <T> supertype of elements of the list
+     * @return list of elements generated by applying given function to list elements
+     * @throws InterruptedException if any thread computing result has interrupted the current thread.
+     */
     public <T, U> List<U> map(int i, List<? extends T> list, Function<? super T, ? extends U> function) throws InterruptedException {
         BiFunction<List<U>, List<U>, List<U>> f = (a, b) -> {
             a.addAll(b);
             return a;
         };
-        BiFunction<Integer, Integer, Supplier<List<U>>> g = (l, r) -> () -> {
+        Function<List<? extends T>, Supplier<List<U>>> g = (subList) -> () -> {
             List<U> res = new ArrayList<>();
-            for (int j = l; j < r; j++) {
-                res.add(function.apply(list.get(j)));
+            for (T element : subList) {
+                res.add(function.apply(element));
             }
             return res;
         };
-        ParallelInvoker<List<U>> parallelInvoker = new ParallelInvoker<>(i, list.size(), g);
-        return parallelInvoker.getAll(new Monoid<>(f));
+        ParallelInvoker<List<U>> parallelInvoker = new ParallelInvoker<>(i, list, g);
+        return parallelInvoker.get(new Monoid<>(f));
     }
 }
