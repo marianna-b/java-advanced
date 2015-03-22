e47f52fd255783e2714cd16f93582495c6977396
mariashka <bisyarinamariashka@gmail.com>
2015-03-22 12:22:08 +0300

    Add binary function to process results

--- src/ru/ifmo/ctddev/bisyarina/concurrent/ParallelInvoker.java
+++ src/ru/ifmo/ctddev/bisyarina/concurrent/ParallelInvoker.java
@@ -1,8 +1,9 @@
 package ru.ifmo.ctddev.bisyarina.concurrent;
 
-import java.lang.reflect.Array;
-import java.util.function.Function;
+import java.util.function.BiFunction;
 import java.util.function.Supplier;
+
+import static java.lang.reflect.Array.newInstance;
 
 public class ParallelInvoker <T> {
     Thread[] threads;
@@ -11,26 +12,23 @@ public class ParallelInvoker <T> {
     @SafeVarargs
     ParallelInvoker(int maxAmount, T ... args) {
         threads = new Thread[maxAmount];
-        results = (T[]) Array.newInstance(args.getClass().getComponentType(), maxAmount);
+        results = (T[]) newInstance(args.getClass().getComponentType(), maxAmount);
         size = 0;
     }
 
     void add(Supplier<? extends T> supplier) {
         int curr_size = size;
-        threads[size] = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                results[curr_size] = supplier.get();
-            }
+        threads[size] = new Thread(() -> {
+            results[curr_size] = supplier.get();
         });
         threads[size].start();
         size++;
     }
 
-    T getAll(Function<T[], T> f) throws InterruptedException {
+    T getAll(BiFunction<T[], Integer, T> f) throws InterruptedException {
         for (int i = 0; i < size; i++) {
             threads[i].join();
         }
-        return f.apply(results);
+        return f.apply(results, size);
     }
 }
