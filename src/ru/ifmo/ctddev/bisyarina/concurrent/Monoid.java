cdf8652dfbc593f9168eb025e42166f2a686e9bf
mariashka <bisyarinamariashka@gmail.com>
2015-03-22 14:46:20 +0300

    Add javadoc

--- src/ru/ifmo/ctddev/bisyarina/concurrent/Monoid.java
+++ src/ru/ifmo/ctddev/bisyarina/concurrent/Monoid.java
@@ -2,31 +2,55 @@ package ru.ifmo.ctddev.bisyarina.concurrent;
 
 import java.util.function.BiFunction;
 
+/**
+ * {@link ru.ifmo.ctddev.bisyarina.concurrent.Monoid} provides functionality
+ * for algebraic structure with a single associative binary operation and an identity element
+ * @param <T>
+ */
+
 public class Monoid<T> {
     private BiFunction<T, T, T> f;
-    private boolean neutral;
+    private boolean identity;
     private T result;
 
+    /**
+     * Creates monoid for cases when identity element is unknown
+     * @param f binary function to process elements
+     */
     Monoid(BiFunction<T, T, T> f) {
         this.f = f;
-        neutral = true;
+        identity = true;
     }
 
-    Monoid(BiFunction<T, T, T> f, T neutralElem) {
+    /**
+     * Creates monoid for cases when identity element is known
+     * @param f binary function to process elements
+     * @param identityElem identity element
+     */
+    Monoid(BiFunction<T, T, T> f, T identityElem) {
         this.f = f;
-        this.neutral = false;
-        this.result = neutralElem;
+        this.identity = false;
+        this.result = identityElem;
     }
 
+    /**
+     * Updates value stored in monoid with given
+     * @param elem element to update with
+     */
     public void process(T elem) {
-        if (neutral) {
+        if (identity) {
             result = elem;
-            neutral = false;
+            identity = false;
             return;
         }
         result = f.apply(result, elem);
     }
 
+    /**
+     * Getter for value stored in monoid.
+     * If stored unknown identity element return null
+     * @return value stored in monoid
+     */
     public T get() {
         return result;
     }
