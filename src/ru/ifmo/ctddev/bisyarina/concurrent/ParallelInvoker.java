9655a44ce11fb7751a6d000115a696bb31035326
mariashka <bisyarinamariashka@gmail.com>
2015-03-22 14:45:36 +0300

    Change declaration and add javadoc

--- src/ru/ifmo/ctddev/bisyarina/concurrent/ParallelInvoker.java
+++ src/ru/ifmo/ctddev/bisyarina/concurrent/ParallelInvoker.java
@@ -1,33 +1,53 @@
 package ru.ifmo.ctddev.bisyarina.concurrent;
 
-import java.util.function.BiFunction;
+import java.util.List;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 import static java.lang.reflect.Array.newInstance;
 
+/**
+ * Provides functionality to process list and get result of specified type
+ * @param <T> type of result
+ */
 public class ParallelInvoker <T> {
-    Thread[] threads;
-    T[] results;
-    int size;
+    private Thread[] threads;
+    private T[] results;
+    private int size;
+
+    /**
+     * Constructs parallel invoker for given amount of threads, given list, and given function generating supplier
+     * for sublist
+     * @param maxAmount maximal amount of threads to use
+     * @param list list to process
+     * @param f function invoked to get answers fornsublists
+     * @param <K> type of list elements
+     */
     @SafeVarargs
-    ParallelInvoker(int maxAmount, int listSize, BiFunction<Integer, Integer, Supplier<T>> f, T ... args ) {
+    public <K> ParallelInvoker(int maxAmount, List<? extends K> list, Function<List<? extends K>, Supplier<T>> f, T ... args ) {
         threads = new Thread[maxAmount];
         //noinspection unchecked
         results = (T[]) newInstance(args.getClass().getComponentType(), maxAmount);
         size = 0;
 
-        int interval = listSize / maxAmount + (listSize % maxAmount == 0 ? 0 : 1);
-        for (int l = 0; l < listSize; l += interval) {
-            int r = Math.min(listSize, l + interval);
+        int interval = list.size() / maxAmount + (list.size() % maxAmount == 0 ? 0 : 1);
+        for (int l = 0; l < list.size(); l += interval) {
+            int r = Math.min(list.size(), l + interval);
             int curr_size = size;
-            Supplier<T> supplier = f.apply(l, r);
+            Supplier<T> supplier = f.apply(list.subList(l, r));
             threads[size] = new Thread(() -> results[curr_size] = supplier.get());
             threads[size].start();
             size++;
         }
     }
 
-    T getAll(Monoid<T> monoid) throws InterruptedException {
+    /**
+     * Returns value computed by given monoid from results of parallel computing
+     * @param monoid monoid to compute result
+     * @return value computed by given monoid
+     * @throws InterruptedException if any thread computing result has interrupted the current thread.
+     */
+    public T get(Monoid<T> monoid) throws InterruptedException {
         for (int i = 0; i < size; i++) {
             threads[i].join();
             monoid.process(results[i]);
