b6c24ee0f91803ad447590290d358cea03f35b35
mariashka <bisyarinamariashka@gmail.com>
2015-04-16 15:10:00 +0300

    Add lib

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-javac -cp ./ParallelMapperTest.jar: src/ru/ifmo/ctddev/bisyarina/concurrent/*.java
+javac -cp ./WebCrawlerTest.jar:./jsoup-1.8.1.jar:./src: src/ru/ifmo/ctddev/bisyarina/crawler/*.java
 
-java -cp ./ParallelMapperTest.jar::./src:./junit-4.11.jar:./quickcheck-0.6.jar:./hamcrest-core-1.3.jar info.kgeorgiy.java.advanced.mapper.Tester list ru.ifmo.ctddev.bisyarina.concurrent.ParallelMapperImpl,ru.ifmo.ctddev.bisyarina.concurrent.IterativeParallelism $1
+java -cp ./WebCrawlerTest.jar:./jsoup-1.8.1.jar:./src:./junit-4.11.jar:./quickcheck-0.6.jar:./hamcrest-core-1.3.jar info.kgeorgiy.java.advanced.crawler.Tester hard ru.ifmo.ctddev.bisyarina.crawler.WebCrawler
