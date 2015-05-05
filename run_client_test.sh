0a02074b0c447ef9479a734783fe02d41ed13e8c
mariashka <bisyarinamariashka@gmail.com>
2015-05-05 16:33:02 +0300

    Fix scripts

--- run_client_test.sh
+++ run_client_test.sh
@@ -2,5 +2,4 @@
 
 javac -cp ./HelloUDPTest.jar:./jsoup-1.8.1.jar:./src: src/ru/ifmo/ctddev/bisyarina/helloudp/*.java
 
-java -cp ./HelloUDPTest.jar:./jsoup-1.8.1.jar:./src:./junit-4.11.jar:./quickcheck-0.6.jar:./hamcrest-core-1.3.jar info.kgeorgiy.java.advanced.hello.Tester client ru.ifmo.ctddev.bisyarina.helloudp.HelloUDPClient $2
-
+java -cp ./HelloUDPTest.jar:./jsoup-1.8.1.jar:./src:./junit-4.11.jar:./quickcheck-0.6.jar:./hamcrest-core-1.3.jar info.kgeorgiy.java.advanced.hello.Tester client ru.ifmo.ctddev.bisyarina.helloudp.HelloUDPClient $1
--- run_server_test.sh
+++ run_server_test.sh
@@ -2,5 +2,4 @@
 
 javac -cp ./HelloUDPTest.jar:./jsoup-1.8.1.jar:./src: src/ru/ifmo/ctddev/bisyarina/helloudp/*.java
 
-java -cp ./HelloUDPTest.jar:./jsoup-1.8.1.jar:./src:./junit-4.11.jar:./quickcheck-0.6.jar:./hamcrest-core-1.3.jar info.kgeorgiy.java.advanced.hello.Tester server ru.ifmo.ctddev.bisyarina.helloudp.HelloUDPServer $2
-
+java -cp ./HelloUDPTest.jar:./jsoup-1.8.1.jar:./src:./junit-4.11.jar:./quickcheck-0.6.jar:./hamcrest-core-1.3.jar info.kgeorgiy.java.advanced.hello.Tester server ru.ifmo.ctddev.bisyarina.helloudp.HelloUDPServer $1
